17dcfb759a2254f03a52bb31a54755affe2716df
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 03:15:09 +0200

    create first component

---
+++ src/AppClassComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class AppClassComponent extends React.Component{
+
+        render(){
+            return(<h1>I'm class compntent</h1>)
+        }
+ 
+}
+
+
+
+export default AppClassComponent
