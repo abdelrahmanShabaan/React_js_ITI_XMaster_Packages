bf1aee7c95e8035174675ca76cee1be6bc4233fe
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 03:26:16 +0200

    use constructor with my component , super fun to call all components i make call from extend

--- src/AppClassComponent.js
+++ src/AppClassComponent.js
@@ -3,6 +3,12 @@ import React from "react";
 
 class AppClassComponent extends React.Component{
 
+
+                constructor(){
+                // that's make call for all components that i make extend from it
+                    super();
+                }
+
         render(){
             return(<h1>I'm class compntent</h1>)
         } 
