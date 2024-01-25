649f5a36de9325107e5009a78df442bc33078e3c
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 03:30:55 +0200

    componentDidMount and others like constructors

--- src/AppClassComponent.js
+++ src/AppClassComponent.js
@@ -4,13 +4,32 @@ import React from "react";
 class AppClassComponent extends React.Component{
 
 
-                constructor(){
-                // that's make call for all components that i make extend from it
-                    super();
-                }
+    constructor(){
+        console.log("hello constructor");
+    // that's  make call for all components that i make extend from it
+        super();
+    }
 
-        render(){
-            return(<h1>I'm class compntent</h1>)
+
+        componentDidMount(){
+            // when load class like constructor 
+            // document.ready (best example to use APIS)
+            console.log("hello did");
+        }
+
+        componentDidUpdate(){
+            // when load class like constructor 
+            console.log("hello update");
+        }
+
+        componentWillUnmount(){
+            // when load class like constructor 
+            console.log("hello mount");
+        }
+    
+
+    render(){
+        return(<h1>I'm class compntent</h1>)
         } 
  
 }
