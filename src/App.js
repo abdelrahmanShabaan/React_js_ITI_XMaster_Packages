1ffe33381a191ec55415684f6a096ad7150f88e4
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 01:20:57 +0200

    added alert react bootstrap

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App"> 
       <h1>Hello News Page</h1>
       <Button variant="primary">Primary</Button>{' '}
-      
+      <Alert variant="info"> alert—check it out!</Alert>
       <AppClassComponent />
       <MyButton name="Hello from app component" color="red"/> 
       <MyTitle title= "I am from apps"/>
