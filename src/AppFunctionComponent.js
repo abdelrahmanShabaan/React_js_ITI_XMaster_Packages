095a6a3622f3a62bb5a6c4dfe47d7b3e5e2aa6b7
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 01:35:46 +0200

    added variable like json style with useState

--- src/AppFunctionComponent.js
+++ src/AppFunctionComponent.js
@@ -7,11 +7,22 @@ function AppFunctionComponent(){
     // define first variable 
     //  state setState
     const[name, setName] =  useState("ahmed") 
+    const [info, setInfo] = useState({
+        name: "ali" ,
+        age :33,
+        city : "6 th october"
+    })
 
+
+
+    // output
     return(
         <>
         <h1>I'm Function Component</h1>
-        <p>my name is {name} </p>
+        <p className="text-primary">my name is {name} </p>
+        <h2> My name: {info.name}</h2>
+        <h2> My age: {info.age}</h2>
+        <h2> My city: {info.city}</h2>
         </>
     )
 
