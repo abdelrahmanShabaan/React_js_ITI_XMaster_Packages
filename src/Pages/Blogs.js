2c3826cd86fcb247f70e9999aeda21ee2f926482
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-28 00:21:26 +0200

    fetch new new apis with useEffect and axios

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,23 +1,31 @@
+import { useEffect, useState } from "react";
 import MyTitle from "../Components/MyTitle";
-
+import axios from "axios";
+import Cart from "../Components/Cart";
 
 
 function Blogs() {
 
+         // Save data
+    const [blogs, setBlogs] = useState([])
 
-            return(
+    // Call API
+    useEffect(() => {
+        axios.get("https://newsapi.org/v2/everything?q=food&apiKey=681cc0004a6e4c13862d656ae468bdcc")
+            .then((res)=> {setBlogs(res.data.articles)})
+            .catch((err) => console.log(err));
+        },[])
 
-
-                <>
-
-                        <MyTitle title="Blogs" />
-                </>
-            )
-
-
-
-}
-
-
-
-export default Blogs;
+        return (
+            <>
+                <MyTitle title="Blogs" />
+    
+                {blogs.map(blog => (
+                    <Cart key={blog.title} name={blog.author} email={blog.title} />
+                ))}
+            </>
+        );
+    }
+    
+    export default Blogs;
+    
