4506ac06d43afeda303066e19a629667f9015b37
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 02:34:01 +0200

    add form

--- src/AppUser.js
+++ src/AppUser.js
@@ -6,7 +6,23 @@ function AddUser(){
 
     return(
         <>
-        <h1 className="text-primary text-center">Add User</h1></>
+        <div className="container">
+             <h1 classNameName="text-primary text-center">Add User</h1>
+            <form>
+            <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Name</label>
+            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            </div>
+
+            <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Position</label>
+            <input type="text" className="form-control" id="exampleInputPassword1"/>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </div>
+            </>
     )
 
 }
