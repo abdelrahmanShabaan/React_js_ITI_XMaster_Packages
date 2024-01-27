8cc9b9e799048d14c97cdb7d738c53c39e727027
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 22:04:40 +0200

    FETCH DATAFROM USER LIST

--- src/Pages/UserDetails.js
+++ src/Pages/UserDetails.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 
 
@@ -5,11 +8,31 @@
 function UserDetails() {
 
 
+            //SAVE THE USER ID From Use params
+                const userId = useParams();
+                //console.log(userId);
+            const [userInfo , setUserInfo] = useState({})
+
+                //useEffect to loop in the data
+                useEffect((users)=> {
+                    //then axios
+                    axios(`https://retoolapi.dev/rPDRQU/data/${userId.id}`)
+                    .then((res) => {setUserInfo(res) })
+                    .catch((err) => {console.log(err)})
+
+                },[])
+
+                
+
+
+
             return(
 
                 <>
                     
                 <h1 className="text-center text-primary">Users Detail</h1>
+
+
 
                 
                 </>
