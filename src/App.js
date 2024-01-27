54eea3403020ac509a6188a5cd729ab69344cb11
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 02:29:05 +0200

    AddUser new Component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import AppFunctionComponent from './AppFunctionComponent';
+import AddUser from './AppUser';
 
 function App() {
   return (
     <div className="App"> 
-      <h1>Hello News Page</h1>
-      <Button variant="primary">Primary</Button>{' '}
+     {/* <h1>Hello News Page</h1>
+       <Button variant="primary">Primary</Button>{' '}
       <Alert variant="info"> alert—check it out!</Alert>
       <AppClassComponent />
       <MyButton name="Hello from app component" color="red"/> 
       <MyTitle title= "I am from apps"/>
-      <AppFunctionComponent />
+      <AppFunctionComponent /> */}
+      <AddUser />
     </div> 
   );
 }
---
+++ src/AppUser.js
@@ -0,0 +1,15 @@
+
+
+
+function AddUser(){
+
+
+    return(
+        <>
+        <h1 className="text-primary text-center">Add User</h1></>
+    )
+
+}
+
+
+export default AddUser;
