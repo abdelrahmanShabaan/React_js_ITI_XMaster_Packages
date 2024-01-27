2b6da3c1d6940cd6fb3eae8852723c2283a67821
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-27 21:26:41 +0200

    apis use

--- src/Pages/UsersList.js
+++ src/Pages/UsersList.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect } from "react";
 
 
 
@@ -5,10 +7,22 @@
 
 function UsersList(){
 
+    //when the page is load --> Call Api --> life Cycle
+    // method ----> didMount --->useEffect 
+    //promises --->resolve reject
+    useEffect(() => {
+
+            //axios.
+            //get 
+            //put 
+            //delete 
+            axios.get("https://retoolapi.dev/2w0eRR/data").
+            then((res) => console.log(res))
+            .catch((err) => console.log(err))
+        },[])
+
+
     return(
-
-
-
         <>
          <h1 className="text-center text-primary">Users List</h1>
         
