d88fa57af99f772f7494f3524c8fc09460fca32d
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-30 02:01:46 +0200

    composeWithDevTools

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,8 +1,9 @@
 import { createStore } from "redux";
 import languageReducer from "./Reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
 
-const mystore = createStore(languageReducer)
+const mystore = createStore(languageReducer , composeWithDevTools)
 
 export default mystore;
