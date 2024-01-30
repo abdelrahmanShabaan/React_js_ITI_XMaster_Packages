8a71932076145394f19b715d2ef42ce6d87fb8dc
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-30 20:55:19 +0200

    dispatch({                     type : "GET_USERS",                     payload : res.data                 })

--- src/Store/Actions/UsersListAction.js
+++ src/Store/Actions/UsersListAction.js
@@ -4,7 +4,14 @@ import axios from "axios"
 //call api ==> Axios
 export const getUsersList = (data) => (dispatch) =>{
 
-    
     return  axios.get("https://retoolapi.dev/rPDRQU/data")
-
+    .then((res) => {
+                //WHEN BACK RES
+                dispatch({
+                    type : "GET_USERS",
+                    payload : res.data
+                })
+        
+    })
+    .catch((err) => console.log(err))
 }
